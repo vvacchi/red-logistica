06d07a7371bf6a35b75b461ea4c14f4939f4045f
vvacchi <205074292+vvacchi@users.noreply.github.com>
2025-10-14 14:54:53 -0300

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ red-logistica/red-logistica/src/main/java/com/uade/tpo/red_logistica/RedLogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.red_logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedLogisticaApplication.class, args);
+	}
+
+}
---
+++ red-logistica/red-logistica/src/test/java/com/uade/tpo/red_logistica/RedLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.red_logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
