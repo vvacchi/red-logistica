f97bbfb47a86f3c7fcf551eab615f211897c3f55
vvacchi <205074292+vvacchi@users.noreply.github.com>
2025-11-18 01:30:04 -0300

    sidebar component

---
+++ red-logistica-ui/src/components/layout/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useAlgorithm } from "../../context/AlgorithmContext";
+
+export default function Sidebar() {
+  const { algorithm, setAlgorithm } = useAlgorithm();
+
+  const algorithms = [
+    { id: "dijkstra", name: "Dijkstra" },
+    { id: "bfs", name: "BFS" },
+    { id: "dfs", name: "DFS" },
+    { id: "greedy", name: "Greedy" },
+    { id: "mergesort", name: "MergeSort" },
+    { id: "pd", name: "Programación Dinámica" },
+    { id: "backtracking", name: "Backtracking" },
+    { id: "branchbound", name: "Branch & Bound" }
+  ];
+
+  return (
+    <aside className="sidebar">
+      <h3>Algoritmos</h3>
+      <ul>
+        {algorithms.map(a => (
+          <li
+            key={a.id}
+            className={algorithm === a.id ? "active" : ""}
+            onClick={() => setAlgorithm(a.id)}
+          >
+            {a.name}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
